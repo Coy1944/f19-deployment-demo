d4b6a35927561b229173fdddff69592dd44c50d3
Coy1944 <Samuelchincks86@gmail.com>
2022-03-16 14:11:34 -0600

    update

--- server/server.js
+++ server/server.js
@@ -1,19 +1,30 @@
-const express = require('express');
-const cors = require('cors');
-const path = requrie('path');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../index.html'));
-});
+// Middleware
+app.use(express.static(path.join(__dirname, "../public")));
+app.use("/images", express.static(path.join(__dirname, "../images")));
+
+// Endpoints
+// app.get("/", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public"));
+// });
+
+// app.get("/", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/index.html"));
+// });
+
+// app.get("/styles", function(req, res) {
+//     res.sendFile(path.join(__dirname, "../public/index.css"));
+// });
 
 const port = process.env.PORT || 4005;
 
 app.listen(port, () => {
     console.log(`We vibin on port ${port}`);
-
 });
-
--- solution/server/index.js
+++ solution/server/index.js
@@ -1,7 +1,10 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
 
-const app = express()
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 app.get('/',function(req,res) {
   res.sendFile(path.join(__dirname, '../index.html'));
